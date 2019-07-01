bbba87a214fd614c3a23786d030ce3099b2d719e
GabrielRodriguez66 <gabriel.rodriguez66@upr.edu>
2019-07-01 16:46:21 -0400

    Added a name field to Chat model, added a Chats view

--- social/urls.py
+++ social/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'social'
 urlpatterns = [
-    path("chat/<int:friend_pk>/<str:view>/", views.chat_manager, name="chat"),
-    path("chat/<int:friend_pk>/<str:view>/<int:chat_pk>/", views.chat_manager, name="new_chat"),
+    path("chat/<int:friend_pk>/<str:view>/<int:chat_pk>/", views.message_manager, name="new_chat"),
     path("open_chat/<int:message_id>/", views.open_chat_view, name="open_chat"),
+    path("chats/", views.all_chats_view, name="chats"),
     path("friends/", views.friends_view, name="friends"),
     path('timeline/', views.timeline, name='timeline'),
     path('search/', views.search, name="search"),
