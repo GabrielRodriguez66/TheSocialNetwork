c7ed99b9748b3692fadb322bf704aa7d002e0b25
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-20 12:34:10 +0800

    Delete social directory

--- social/static/social/login.js
+++
@@ -1,8 +0,0 @@
-function showPass(){
-    let x = document.getElementById("id_password");
-    if (x.type == "password"){
-        x.type = "text";
-    }else{
-        x.type = "password";
-    }
-}
